43d3e383a5ff812288efccd1fe6f394a8b755040
Kim Tae Wook <gmill0411@naver.com>
2024-12-17 16:39:21 +0900

    System Clock

---
+++ CPP/SystemClock/SystemClock.cpp
@@ -0,0 +1,23 @@
+﻿// SystemClock.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+
+//int main()
+//{
+//    std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+//    std::time_t t = std::chrono::system_clock::to_time_t(now);
+//    std::cout << "현재 시간은 : " << std::put_time(std::localtime(&t), "%F %T %z") << '\n';
+//}
+
+int main()
+{
+    std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+    std::time_t t = std::chrono::system_clock::to_time_t(now);
+    std::tm tm_info;
+    localtime_s(&tm_info, &t);
+    std::cout << "현재 시간은 : " << std::put_time(&tm_info, "%F %T %z") << '\n';
+}

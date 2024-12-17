a1968bfbd4fe7d43fdf881894924dffa65aa554a
Kim Tae Wook <gmill0411@naver.com>
2024-12-17 16:31:45 +0900

    RValueRef

---
+++ CPP/RValueRef/RValueRef.cpp
@@ -0,0 +1,36 @@
+﻿// RValueRef.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+class A
+{
+public:
+    A()
+        : num(0)
+    {
+        std::cout << "기본 생성자" << std::endl;
+    }
+
+    A(const A& _Other)
+        : num(_Other.num)
+    {
+        std::cout << "복사 생성자" << std::endl;
+    }
+
+    A(A&& _Value)
+    {
+        std::cout << "이동 생성자" << std::endl;
+    }
+
+private:
+    int num;
+};
+
+int main()
+{
+    A a = A();
+    A b = A(a);
+
+    A c = A(std::move(b));
+}

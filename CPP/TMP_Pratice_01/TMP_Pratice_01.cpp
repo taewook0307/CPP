f6dae2bf6790f0f14dd67c32206b1709e10d523b
Kim Tae Wook <gmill0411@naver.com>
2024-12-17 16:51:18 +0900

    TMP

---
+++ CPP/TMP_Pratice_01/TMP_Pratice_01.cpp
@@ -0,0 +1,27 @@
+﻿// TMP_Pratice_01.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <type_traits>
+
+template <typename T>
+void GetType()
+{
+    if (std::is_void<T>::value)
+    {
+        std::cout << "T 는 void ! \n";
+    }
+    else
+    {
+        std::cout << "T 는 void 가 아니다. \n";
+    }
+}
+
+int main()
+{
+    int a = 3;
+    int b = 5;
+
+    GetType<int>();
+    GetType<void>();
+}

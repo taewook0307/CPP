0684d32eb2a230794bda63edd2fb7ebfb1a09c94
Kim Tae Wook <gmill0411@naver.com>
2024-12-17 16:21:14 +0900

    Double Free

---
+++ CPP/DoubleFree/DoubleFree.cpp
@@ -0,0 +1,24 @@
+﻿// DoubleFree.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+class A
+{
+public:
+	A()
+		: a(0)
+	{
+
+	}
+
+private:
+	int a;
+};
+
+int main()
+{
+	A* PA = new A();
+	delete PA;
+	// delete PA;
+}

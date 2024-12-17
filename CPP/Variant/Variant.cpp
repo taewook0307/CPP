ceb32db7ff8e0a662553a7fab0135d479e889a83
Kim Tae Wook <gmill0411@naver.com>
2024-12-17 16:42:13 +0900

    Variant

---
+++ CPP/Variant/Variant.cpp
@@ -0,0 +1,26 @@
+﻿// Variant.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <variant>
+#include <string>
+
+int main()
+{
+	std::variant<int, double, std::string> Test;
+
+	size_t Size = sizeof(Test);
+	std::cout << "Size = " << Size << std::endl;
+
+
+	Test = 4.0;
+
+	Size = sizeof(Test);
+	std::cout << "Size = " << Size << std::endl;
+
+
+	Test = "String";
+
+	Size = sizeof(Test);
+	std::cout << "Size = " << Size << std::endl;
+}

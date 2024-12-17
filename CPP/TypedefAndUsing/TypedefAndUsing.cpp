61be9cb9a662fdf6d4ac92234b140390195cbb16
Kim Tae Wook <gmill0411@naver.com>
2024-12-17 16:37:04 +0900

    Typedef And Using

---
+++ CPP/TypedefAndUsing/TypedefAndUsing.cpp
@@ -0,0 +1,45 @@
+﻿// TypedefAndUsing.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+template <typename T>
+class MyClass
+{
+public:
+    MyClass()
+        : a(0)
+    {
+        std::cout << "MyClass 생성자" << std::endl;
+    }
+
+    MyClass(T _Num)
+        : a(_Num)
+    {
+        std::cout << "MyClass 생성자" << std::endl;
+    }
+
+    ~MyClass()
+    {
+        std::cout << "MyClass 소멸자" << std::endl;
+    }
+
+private:
+    T a = 0;
+};
+
+//template <typename T>
+//typedef MyClass<T> MCTD;
+
+template <typename T>
+using MCU = MyClass<T>;
+
+int main()
+{
+    //MCTD* NewTD = new MCTD();
+
+    MCU<int>* NewU = new MCU<int>();
+
+    delete NewU;
+    //delete NewTD;
+}
